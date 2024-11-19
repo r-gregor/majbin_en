fd7e3f322e680d91f5067d784ac6962b59c0b94f
r-gregor <gredelonghi@gmail.com>
2024-11-19 13:49:27 +0100

    update 20241119_1349_en

--- scales_of_displays_p.py
+++ scales_of_displays_p.py
@@ -3,7 +3,7 @@
 
 names={720:"HD", 1080:"FHD", 1440:"QHD", 2160:"UHD/4K", 4320:"8K"}
 
-print(f'\nFactor  Horisontal Vertical   Name')
+print(f'\nFactor  Horizontal Vertical   Name')
 print(f'-----------------------------------')
 for N1 in [1.0, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0, 6.0]:
 	# N1 = 1 + N/10.0
