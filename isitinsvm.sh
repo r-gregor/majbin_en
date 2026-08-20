79ad698af33f0053cf4e8ae565244222bb5b816d
r-gregor <gredelonghi@gmail.com>
2026-08-20 16:12:08 +0200

    update 20260820_1612_en

---
+++ isitinsvm.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+PTH='/home/gregor.redelonghi/majstaf/engit/prenos/_DSVM.txt'
+
+if [ $# -ne 1 ]; then
+	printf "[ERROR] must supply a part of movie name\n\n"
+	exit
+else
+	PTRN="$1"
+fi
+
+grep -i "$PTRN" "${PTH}"
+
+if [ $? -ne 0 ]; then
+	echo "NOT IN THE _DSVM: ${PTRN}"
+fi
+
+printf "\n"
+
